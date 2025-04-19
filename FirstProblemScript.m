3dfa1439cbe73fc271696ffd61e9bb1d53b85e99
julianbeloiu <julianbeloiu@ufl.edu>
2025-04-19 17:50:29 -0400

    Vectorized the operations

--- FirstProblemScript.m
+++ FirstProblemScript.m
@@ -24,12 +24,12 @@ function out = f(theta)
     A3 = L2 * (cos(theta) * cos(gamma) - sin(theta) * sin(gamma)) - x2;
     B3 = L2 * (cos(theta) * sin(gamma) + sin(theta) * cos(gamma)) - y2;
 
-    N1 = B3 * (p2^2 - p1^2- A2^2 - B2^2) - B2 * (p3^2 - p1^2 - A3^2 - B3^2);
-    N2 = -A3 * (p2^2 - p1^2- A2^2 - B2^2) + A2 * (p3^2 - p1^2 - A3^2 - B3^2);
+    N1 = B3 .* (p2^2 - p1^2- A2.^2 - B2.^2) - B2 .* (p3^2 - p1^2 - A3.^2 - B3.^2);
+    N2 = -A3 .* (p2^2 - p1^2- A2.^2 - B2.^2) + A2 .* (p3^2 - p1^2 - A3.^2 - B3.^2);
 
-    D = 2 * (A2 * B3 - B2 * A3);
+    D = 2 * (A2 .* B3 - B2 .* A3);
 
-    out = N1^2 + N2^2 - p1^2 * D^2;
+    out = N1.^2 + N2.^2 - p1.^2 * D.^2;
 
 end
 
